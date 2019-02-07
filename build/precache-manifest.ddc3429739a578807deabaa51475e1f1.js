0c348d98ddae1d5fb32b43ecc30f190ffa0b890c
nataliasabadysh <nataliasabadysh@gmail.com>
2019-02-07 03:42:10 +0200

    removed from gitignore /build

---
+++ build/precache-manifest.ddc3429739a578807deabaa51475e1f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e73f43f0f42f213e31d8",
+    "url": "/static/js/main.e73f43f0.chunk.js"
+  },
+  {
+    "revision": "64641b8766131f1e3551",
+    "url": "/static/js/1.64641b87.chunk.js"
+  },
+  {
+    "revision": "e73f43f0f42f213e31d8",
+    "url": "/static/css/main.f3b1a61d.chunk.css"
+  },
+  {
+    "revision": "aff17599f66d2175294355f0e5d10f94",
+    "url": "/index.html"
+  }
+];
