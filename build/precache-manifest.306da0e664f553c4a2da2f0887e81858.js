7bf8ad4faacaee44895d17f8e414856903e471b3
nataliasabadysh <nataliasabadysh@gmail.com>
2019-02-07 11:29:40 +0200

    to git page

---
+++ build/precache-manifest.306da0e664f553c4a2da2f0887e81858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "277ce22c06e47f67e1e9",
+    "url": "/YoutubePlayer/build/static/js/runtime~main.277ce22c.js"
+  },
+  {
+    "revision": "7be4e9bd8b1c2514c433",
+    "url": "/YoutubePlayer/build/static/js/main.7be4e9bd.chunk.js"
+  },
+  {
+    "revision": "64641b8766131f1e3551",
+    "url": "/YoutubePlayer/build/static/js/1.64641b87.chunk.js"
+  },
+  {
+    "revision": "7be4e9bd8b1c2514c433",
+    "url": "/YoutubePlayer/build/static/css/main.f3b1a61d.chunk.css"
+  },
+  {
+    "revision": "31f735418ebf0c3dc6e56c773dda965f",
+    "url": "/YoutubePlayer/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddc3429739a578807deabaa51475e1f1.js"
+  "/YoutubePlayer/build/precache-manifest.306da0e664f553c4a2da2f0887e81858.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/YoutubePlayer/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
